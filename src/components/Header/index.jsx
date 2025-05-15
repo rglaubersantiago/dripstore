20ed0195b8ff77fb995b5ce8f5b1502190da95f8
Glauber Santiago <glaubersantiago@outlook.com>
2025-05-14 21:10:00 -0300

    Cabeçalho

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,33 @@
-function Header(){
+import DigitalStore from "../Header/assets/DigitalStore.svg"
+import FlatIcon from "../Header/assets/FlatIcon.svg"
+import Search from '../Header/assets/Search.svg'
 
-    return(
+import './style.css'
+
+function Header() {
+
+    return (
         <>
-            <h1>Olá Mundo</h1>
+            <header className="header">
+                <section className="header__searchMenu">
+                    <div>
+                        <img src={DigitalStore} alt="Logo da digital college" />
+                        <input type="search" placeholder="Pesquisar produto..." />
+                        <img id="lupa" src={Search} alt="Ícone de lupa" />
+                        <a href="#">Cadastre-se</a>
+                        <a id="button-entrar" href="">Entrar</a>
+                        <img id='flat-icon' src={FlatIcon} alt="Icone de carrinho de compras de cor rosa" />
+                    </div>
+                    <nav class='nav-container'>
+                        <ul class="nav-list">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Produtos</a></li>
+                            <li><a href="#">Categorias</a></li>
+                            <li><a href="#">Meus Pedidos</a></li>
+                        </ul>
+                    </nav>
+                </section>
+            </header>
         </>
     )
 }
