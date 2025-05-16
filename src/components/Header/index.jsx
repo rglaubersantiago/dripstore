637030e745ce53c58e0e1a658ceeae81f35663b0
Glauber Santiago <glaubersantiago@outlook.com>
2025-05-16 10:18:52 -0300

    Update cabeçalho

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,24 +8,24 @@ function Header() {
 
     return (
         <>
-            <header className="header">
-                <section className="header__searchMenu">
-                    <div>
-                        <img src={logo} alt="Logo da digital college" />
+            <header id="header">
+                <section id="section">
+                    <div id="search-menu-container">
+                        <img id="logo" src={logo} alt="Logo da digital college" />
                         <input type="search" placeholder="Pesquisar produto..." />
                         <img id="lupa" src={Search} alt="Ícone de lupa" />
                         <a href="#">Cadastre-se</a>
-                        <a id="button-entrar" href="">Entrar</a>
-                        <img id='flat-icon' src={FlatIcon} alt="Icone de carrinho de compras de cor rosa" />
+                        <nav>
+                            <ul id="nav-list">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Produtos</a></li>
+                                <li><a href="#">Categorias</a></li>
+                                <li><a href="#">Meus Pedidos</a></li>
+                            </ul>
+                        </nav>
                     </div>
-                    <nav class='nav-container'>
-                        <ul class="nav-list">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Produtos</a></li>
-                            <li><a href="#">Categorias</a></li>
-                            <li><a href="#">Meus Pedidos</a></li>
-                        </ul>
-                    </nav>
+                    <a id="button-entrar" href="">Entrar</a>
+                    <img id='flat-icon' src={FlatIcon} alt="Icone de carrinho de compras de cor rosa" />
                 </section>
             </header>
         </>
